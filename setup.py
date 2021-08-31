01595d058abc218a2c532b4af6846152717589e4
azam6301732985 <72563306+azam6301732985@users.noreply.github.com>
2021-08-31 09:35:20 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Iris Model Deployment',
+    version='1.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Dell',
+    author_email='azam9550559696@gmail.com',
+    description='Iris Model Deployment'
+)
